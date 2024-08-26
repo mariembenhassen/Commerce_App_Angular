c9be049c4a08308cc8ac95c0096060fddd8d7bec
mariembenhassen <121687274+mariembenhassen@users.noreply.github.com>
2024-08-26 19:10:49 +0100

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { DemoAngularMaterailModule } from './DemoAngularMaterialModule';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -28,12 +29,13 @@ import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule,
+  
     MatCardModule,
     MatSnackBarModule,
     AppRoutingModule,
     DemoAngularMaterailModule,
     HttpClientModule,
+    MatIconModule
   ],
   providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent]

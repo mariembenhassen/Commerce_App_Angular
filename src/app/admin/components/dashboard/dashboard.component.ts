1ff4139c64ff663f8dcd4956c810812bfd6c7881
mariembenhassen <121687274+mariembenhassen@users.noreply.github.com>
2024-08-29 03:08:10 +0100

    delete works

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { title } from 'process';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +12,18 @@ import { AdminService } from '../../service/admin.service';
 export class DashboardComponent {
 
   products: any[] = [];
+  searchProductForm!: FormGroup;
 
-  constructor(private adminService: AdminService){}
+  constructor(private adminService: AdminService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+  ){}
 
   ngOnInit(){
     this.getAllProducts();
+    this.searchProductForm= this.fb.group({
+      title: [null, [Validators.required]]
+    })
   }
 
   getAllProducts(){ 
@@ -24,8 +34,37 @@ export class DashboardComponent {
         this.products.push(element);
       });
     })
-  
-  
+  }
+
+  submitForm(){
+    this.products= [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProductsByName(title).subscribe(res =>{
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products)
+    })
+
+
+  }
+  deleteProduct(productId:any){
+    this.adminService.deleteProduct(productId).subscribe(res =>{
+      if (res === null || res === undefined) {
+        // Assuming successful deletion results in an empty or null response
+        this.snackBar.open('Product Deleted Successfully!', 'Close', {
+          duration: 5000
+        });
+        this.getAllProducts();
+      }else{
+        this.snackBar.open(res.message,'Close', {
+          duration: 5000,
+          panelClass: 'error-snackbar'
+        })
+      }
+    })
+    
   }
 
 
